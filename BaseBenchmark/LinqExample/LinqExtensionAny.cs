3bedd41114ea04f2c078b97cf8e1fb9a7be0f7cc
Pavel <sculltakers111@gmail.com>
2022-07-19 10:46:31 +0300

    second commit

---
+++ BaseBenchmark/LinqExample/LinqExtensionAny.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BaseBenchmark.LinqExample;
+
+[MemoryDiagnoser()]
+public sealed class LinqExtensionAny
+{
+    public List<User> _users = new List<User>();
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            _users.Add(new User());
+        }
+    }
+
+    [Benchmark]
+    public bool ComputeCount()
+    {
+        return _users.Count > 0;
+    }
+    
+    [Benchmark]
+    public bool AnyCount()
+    {
+        return _users.Any();
+    }
+}
